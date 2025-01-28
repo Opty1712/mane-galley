3afb4a6a8f96b01d5d91b5f1013bccc58cef9c75
Opty1712 <opty@opty.ru>
2025-01-28 18:30:06 +0300

    added generateStaticParams

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,19 +1,19 @@
-import { notFound } from 'next/navigation';
+import { notFound } from "next/navigation";
 
 const projects = {
-  'uncle-vpn': {
-    title: 'UncleVPN',
-    description: 'Описание проекта UncleVPN...',
+  "uncle-vpn": {
+    title: "UncleVPN",
+    description: "Описание проекта UncleVPN...",
   },
-  'sber-jazz': {
-    title: 'SberJazz',
-    description: 'Описание проекта SberJazz...',
+  "sber-jazz": {
+    title: "SberJazz",
+    description: "Описание проекта SberJazz...",
   },
-  'kalitniki': {
-    title: 'Калитники',
-    description: 'Описание проекта Калитники...',
+  kalitniki: {
+    title: "Калитники",
+    description: "Описание проекта Калитники...",
   },
-};
+} as const;
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
   const project = projects[params.slug as keyof typeof projects];
@@ -28,4 +28,11 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
       <p className="text-lg text-gray-600">{project.description}</p>
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  // Return an array of objects with the `slug` parameter for each project
+  return Object.keys(projects).map((project) => ({
+    slug: projects[project as keyof typeof projects],
+  }));
 }
