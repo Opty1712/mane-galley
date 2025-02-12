de5f0ab27c4bff64fb3541454c9699244344335c
Opty1712 <opty@opty.ru>
2025-02-12 12:43:45 +0300

    fixed for safari

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -1,16 +1,60 @@
+"use client";
+import { useEffect, useRef } from "react";
 import styles from "./ScrollToTop.module.css";
-export const ScrollToTop = () => (
-  <a href="#content" className={styles.backToTop} title="Прокрутить наверх">
-    <span className={styles.backToTopInner}>Прокрутить наверх</span>
-    <svg
-      width="32"
-      height="32"
-      viewBox=" 0 0 24 24"
-      aria-hidden="true"
-      className={styles.backToTopSVG}
+export const ScrollToTop = () => {
+  const ref = useRef<HTMLAnchorElement>(null);
+
+  useEffect(() => {
+    const isAnimationScrollSupported = CSS.supports(
+      "animation-timeline",
+      "scroll()"
+    );
+
+    if (isAnimationScrollSupported) {
+      return;
+    }
+
+    const paintScrollButton = () => {
+      if (!ref.current) {
+        return;
+      }
+
+      const scrollTop = window.scrollY;
+      const windowHeight = document.body.offsetHeight;
+      const scrollTopRelation = scrollTop / windowHeight;
+      const scrollTopPercentWithThreshold = `${scrollTopRelation * 110}%`;
+
+      ref.current.style.opacity = String(scrollTopRelation * 10);
+
+      ref.current.style.backgroundImage = `linear-gradient(
+        var(--accent-color) 0% ${scrollTopPercentWithThreshold},
+        var(--white) ${scrollTopPercentWithThreshold} 100%
+      )`;
+    };
+
+    window.addEventListener("scroll", paintScrollButton);
+
+    return () => window.removeEventListener("scroll", paintScrollButton);
+  }, []);
+
+  return (
+    <a
+      href="#content"
+      className={styles.backToTop}
+      title="Прокрутить наверх"
+      ref={ref}
     >
-      <path d="M5 12h14" />
-      <path d="m12 5 7 7-7 7" />
-    </svg>
-  </a>
-);
+      <span className={styles.backToTopInner}>Прокрутить наверх</span>
+      <svg
+        width="32"
+        height="32"
+        viewBox=" 0 0 24 24"
+        aria-hidden="true"
+        className={styles.backToTopSVG}
+      >
+        <path d="M5 12h14" />
+        <path d="m12 5 7 7-7 7" />
+      </svg>
+    </a>
+  );
+};
--- app/utils/scrollToSection.ts
+++ app/utils/scrollToSection.ts
@@ -1,6 +1,38 @@
+"use client";
+
 export const scrollToSection = (id: string) => {
   const element = document.getElementById(id);
   if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
+    if (isChromium) {
+      element.scrollIntoView({ behavior: "smooth" });
+    } else {
+      smoothScrollTo(element, 1500);
+    }
   }
 };
+
+const isChromium = "chrome" in window;
+
+/** for non chromium browsers */
+function smoothScrollTo(element: HTMLElement, duration = 1000) {
+  const targetPosition = element.getBoundingClientRect().top + window.scrollY;
+  const startPosition = window.scrollY;
+  const distance = targetPosition - startPosition;
+  let startTime: number | null = null;
+
+  function animation(currentTime: number) {
+    if (startTime === null) startTime = currentTime;
+    const timeElapsed = currentTime - startTime;
+    const progress = Math.min(timeElapsed / duration, 1); // 0 - 1
+    const easeInOutQuad =
+      progress < 0.5
+        ? 2 * progress * progress
+        : 1 - Math.pow(-2 * progress + 2, 2) / 2;
+
+    window.scrollTo(0, startPosition + distance * easeInOutQuad);
+
+    if (timeElapsed < duration) requestAnimationFrame(animation);
+  }
+
+  requestAnimationFrame(animation);
+}
