41db02f23e50de8a32b85d41728fa5bdb7813455
Opty1712 <opty@opty.ru>
2025-02-27 01:03:52 +0300

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -80,15 +80,26 @@ export const Footer = () => {
         )}
       >
         <div className={styles.grid5} />
-        <div className={clsx(styles.grid6)}>
-          ©2025 Манаткина Наталья
-          <FooterCopy />
-        </div>
+        <div className={clsx(styles.grid6)}>©2025 Манаткина Наталья</div>
         <div className={clsx(styles.grid13)}>
           <Typograph>
             Контент предназначен исключительно для личного использования и
             некоммерческих целей
           </Typograph>
+        </div>
+      </div>
+      <div
+        className={clsx(
+          styles.pageGrid,
+          styles.footerText,
+          styles.commonSection
+        )}
+      >
+        <div className={styles.grid5} />
+        <div className={clsx(styles.grid6)}>
+          <FooterCopy />
+        </div>
+        <div className={clsx(styles.grid13)}>
           <div
             className={styles.commonSectionBlock}
             style={{
